96363826dfca91219df6cb2ded5b215bf8a0e923
ismailync <ismailyonca2001@gmail.com>
2022-09-03 19:07:28 +0300

    Add project files.

---
+++ StaticProject/Musteri.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticProject
+{
+    public class Musteri
+    {
+        static ArrayList musteriDatabase;
+
+        static Musteri()
+        {
+            musteriDatabase = new ArrayList();
+        }
+
+        public int MusteriID { get; set; }
+        public String MusteriAd { get; set; }
+        public String MusteriSoyAd { get; set; }
+        public String MusteriEmail { get; set; }
+        public String MusteriSifre { get; set; }
+
+        private String _musteriKullaniciAd;
+
+        public String MusteriKullaniciAd
+        {
+            get
+            {
+                return this._musteriKullaniciAd;
+            }
+            set
+            {
+                bool kullaniciAdiKontrol = musteriKullaniciAdiKontrol(value);
+
+                if (kullaniciAdiKontrol)
+	            {
+                    Console.WriteLine("Bu kullanıcı adı kayıtlı");
+                    this._musteriKullaniciAd= string.Empty;
+	            }
+                else
+                {
+                    this._musteriKullaniciAd = value;
+                }
+            }
+        }
+
+
+        static bool musteriKullaniciAdiKontrol(string _musteriKullaniciAd)
+        {
+            bool Kontrol = false;
+
+            for (int i = 0; i < musteriDatabase.Count; i++)
+			{
+                Musteri Temp = (Musteri)musteriDatabase[i];
+                if (Temp.MusteriKullaniciAd == _musteriKullaniciAd)
+            	{
+                    Kontrol = true;
+                    break;
+	            }
+			}
+            return Kontrol;
+        }
+
+        public  static void MusteriEkle(Musteri M)
+        {
+            if (M != null && !string.IsNullOrEmpty(M.MusteriKullaniciAd) 
+                && !string.IsNullOrEmpty(M.MusteriEmail))
+	        {
+                 bool emailAdresKontrol = musteriEmailAdresKontrol(M.MusteriEmail);
+
+                if (emailAdresKontrol)
+	            {
+                    Console.WriteLine("Bu Email adı kayıtlı");
+	            }
+                else
+                {
+                    musteriDatabase.Add(M);
+                    Console.WriteLine("Kaytı yapıldı");
+                }
+        	}
+        }
+
+        static bool musteriEmailAdresKontrol(string _emailAdres)
+        {
+            bool Kontrol = false;
+
+            for (int i = 0; i < musteriDatabase.Count; i++)
+			{
+                Musteri Temp = (Musteri)musteriDatabase[i];
+                if (Temp.MusteriEmail == _emailAdres)
+            	{
+                    Kontrol = true;
+                    break;
+	            }
+			}
+            return Kontrol;
+        }
+
+
+    }
+}   
---
+++ StaticProject/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri m1 = new Musteri();
+            m1.MusteriID = 1;
+            m1.MusteriAd = "ismail";
+            m1.MusteriSoyAd = "yonca";
+            m1.MusteriEmail = "ismail@gamil.com";
+            m1.MusteriKullaniciAd = "ismail.yonca";
+            m1.MusteriSifre = "1";
+
+            Musteri.MusteriEkle(m1);
+
+
+
+        }
+    }
+}
